9a50f826bbc0a01e14e41d3ad85ecc4afedfe29e
John Peter Neville <neville.peter.2000@gmail.com>
2024-02-09 13:55:57 -0500

    parte 1 del proyecto

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from  "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import  postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+// para la base datos mondodb atlas
+
+const CONNECTION_URL= 'mongodb+srv://memories:johnpeter83@chatbot.iv9fjrd.mongodb.net/?retryWrites=true&w=majority'
+const PORT= process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(()=>app.listen(PORT, ()=> console.log(`Server is running on port: ${PORT}`)))
+  .catch((error)=> console.log(error.message));
+
+
