99e5ccbf4bc59ac29965955ae7af7a3c1dd78f95
John Peter Neville <neville.peter.2000@gmail.com>
2024-02-11 02:50:57 -0500

    se actualiza material ui al proyecto

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"; 
+import Posts from "./Components/Posts/Posts";
+import Form from "./Components/Form/Form";
+import  useStyles from "./styles.js";
+
+import memories from './images/memories.png';
+
 
 const App  = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+
+    <Container maxWidth="lg">
+      <AppBar className={classes.appbar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memories" className={classes.image} height={50} />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="strech" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
-}
+};
 
 export default App;
---
+++ client/src/Components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Form = () =>{
+  return (
+    <h1>Fomr</h1>
+  );
+}
+
+export default Form;
